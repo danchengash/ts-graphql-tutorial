a87491c2cbfbdf11022a3b3181fcaf49abedce13
Genialngash <ngangadanche@gmail.com>
2023-05-10 17:59:01 +0300

    server

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
+
+
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+  // The ApolloServer constructor requires two parameters: your schema
+// npm install @apollo/server graphql
+
+
+interface MyContext {
+  token?: String;
+}
+
+const server = new ApolloServer<MyContext>({ typeDefs, resolvers });
+const { url } = await startStandaloneServer(server, {
+  context: async ({ req }) => ({ token: req.headers.token }),
+  listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at ${url}`);
