5767276044ceb39221ee7fe28107f75c7248e443
Genialngash <ngangadanche@gmail.com>
2023-05-11 16:28:26 +0300

    re

---
+++ models/books.ts
@@ -0,0 +1,20 @@
+import { Schema,model } from "mongoose";
+interface TBook {
+    id?:string,
+    author: string,
+    title: string,
+    year: Number,
+}
+
+const BookSchema = new Schema<TBook>({
+    id: String,
+    author: {type: String, required: true},
+    title: {type: String, required: true},
+
+    year: {type: String, required: true},
+
+
+});
+
+const  Book = model<TBook>('books',BookSchema)
+export default Book
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,31 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import {connect} from 'mongoose'
+import {Book}from '../models/books.js';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+const MONGODB = 'mongodb+srv://localhost:27018/apollo-graph';
+
 const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-
-
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-    Query: {
-      books: () => books,
-    },
-  };
-
-
-
-
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
-
-  // The ApolloServer constructor requires two parameters: your schema
-// npm install @apollo/server graphql
-
-
-interface MyContext {
-  token?: String;
+type Book{
+     _id :string,
+    author: string,
+    title: string,
+    year: Int,
 }
 
-const server = new ApolloServer<MyContext>({ typeDefs, resolvers });
-const { url } = await startStandaloneServer(server, {
-  context: async ({ req }) => ({ token: req.headers.token }),
-  listen: { port: 4000 },
-});
-console.log(`🚀  Server ready at ${url}`);
+input BookInput {
+  author: string,
+    title: string,
+    year: Int,
+}
+
+type Query {
+  getBook(ID:ID!):Book!
+  getBooks(limit:int):[Book]
+}
+type Mutation {
+  createBook(bookInput: BookInput):String!
+  updateBook(ID:ID!,bookInput:BookInput): String!
+  deleteBook(ID:ID!):String!
+}
+`;
